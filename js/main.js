3dd490e572b63a9c202058df7ab1fe3d89771aa8
Xingzawa <xingzawa@gmail.com>
2026-06-29 08:55:43 +0800

    Add clock update functionality to main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+function updateClock(){
 
+    const now=new Date();
+
+    let h=now.getHours().toString().padStart(2,"0");
+    let m=now.getMinutes().toString().padStart(2,"0");
+
+    document.getElementById("clock").textContent=h+":"+m;
+
+}
+
+updateClock();
+
+setInterval(updateClock,1000);
